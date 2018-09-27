d2632e02351583ef97793b13c1806657ea485223
Tim Williams <jtimwill@gmail.com>
2018-09-27 12:04:23 -0400

    Update the generateAuthToken method to include name and email

--- models/user.js
+++ models/user.js
@@ -28,7 +28,10 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.generateAuthToken = function() {
   const secret = config.get('jwt_private_key');
   return jwt.sign({ 
-    _id: this._id, admin: this.admin 
+    _id: this._id, 
+    name: this.name,
+    email: this.email,
+    admin: this.admin 
   }, secret/*, { expiresIn: '1h' }*/);
 };
 
